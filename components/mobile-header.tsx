4c411ebc988d722c46271ac75ea09ec41ca2a851
ArinLamba <arinlamba5@gmail.com>
2025-05-16 02:08:19 +0530

    Logo on header

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -7,8 +7,8 @@ export const MobileHeader = () => {
     <nav className="lg:hidden px-6 h-[50px] flex items-center bg-green-600 border-b fixed top-0 w-full z-50">
       <MobileSidebar />
       <div className="left-1/2 absolute -translate-x-1/2 flex items-center gap-x-3">
-        <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
-        <h1 className="text-2xl font-extrabold text-muted tracking-wide">
+        <Image src="/mascot.svg" height={30} width={30} alt="Mascot" />
+        <h1 className="text-xl font-extrabold text-muted tracking-wide">
           Lingo
         </h1>
       </div>
